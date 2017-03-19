dfb5449239fcec6e85c8109d11128e76f799bdf9
yba <yba@umich.edu>
2017-03-19 16:06:32 -0400

    Valve: update arduino code for 4 pins

--- valve/arduino.ino
+++ valve/arduino.ino
@@ -7,21 +7,21 @@ void loop() {
   if (Serial.available()) {
     char c = Serial.read();
     if (c == '1') {
-      analogWrite(9, 1023);
+      analogWrite(8, 1023);
     } else if (c == '2') {
-      analogWrite(9, 0);
+      analogWrite(8, 0);
     } else if (c == '3') {
       analogWrite(9, 1023);
     } else if (c == '4') {
       analogWrite(9, 0);
     } else if (c == '5') {
-      analogWrite(9, 1023);
+      analogWrite(10, 1023);
     } else if (c == '6') {
-      analogWrite(9, 0);
+      analogWrite(10, 0);
     } else if (c == '7') {
-      analogWrite(9, 1023);
+      analogWrite(11, 1023);
     } else if (c == '8') {
-      analogWrite(9, 0);
+      analogWrite(11, 0);
     }
   }
 }
