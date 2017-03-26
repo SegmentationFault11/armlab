d566df2c448958b7dd3614d94b513516163c0c77
Steven Ma <7348348564@stevenma.xyz>
2017-03-25 21:55:38 -0400

    Add AprilTag dependencies for bottle_recognizer

--- bottle_recognition/bottle_recognizer.hpp
+++ bottle_recognition/bottle_recognizer.hpp
@@ -1,10 +1,39 @@
 #pragma once
 
+#ifdef __LINUX__
+#define EXPOSURE_CONTROL 
+#endif
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <sys/time.h>
+#include <string>
+#include <cmath>
+#include <unistd.h>
+
+#ifdef EXPOSURE_CONTROL
+#include <libv4l2.h>
+#include <linux/videodev2.h>
+#include <fcntl.h>
+#include <errno.h>
+#endif
+
+#include "opencv2/opencv.hpp"
+
+#include "AprilTags/TagDetector.h"
+#include "AprilTags/Tag16h5.h"
+#include "AprilTags/Tag25h7.h"
+#include "AprilTags/Tag25h9.h"
+#include "AprilTags/Tag36h9.h"
+#include "AprilTags/Tag36h11.h"
+
 using namespace std;
 
 typedef class BottleRecognizer {
 public:
 
-	string get_locations();
-	
+    string get_locations();
+    
 } bottle_recognizer_t;
