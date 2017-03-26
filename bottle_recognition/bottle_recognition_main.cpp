4707f2f64998c8764e8a707f136294c27d52bf74
Steven Ma <7348348564@stevenma.xyz>
2017-03-25 20:38:04 -0400

    Add basic socket functionality to main

--- bottle_recognition/bottle_recognition_main.cpp
+++ bottle_recognition/bottle_recognition_main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <string>
+
+#include "socket_functions.hpp"
 
 using namespace std;
 
 int main() {
+
+    int soc = setup_socket(12000);
+
+    int client_soc = -1;
+    while ((client_soc = accept_socket(soc))) {
+
+        if (client_soc == -1) {
+            cerr << "Invalid socket" << endl;
+            exit(1);
+        }
+
+        string msg = read_msg(client_soc);
+
+        cout << "Reply: " << endl;
+
+        string reply;
+        cin >> reply;
+
+        send_msg(client_soc, reply);
+    }
 
     return 0;
 }
