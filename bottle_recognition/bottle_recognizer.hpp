659002e34fe5f90b2fed0ae5e52b404f4efc5c8f
Steven Ma <7348348564@stevenma.xyz>
2017-03-26 11:47:51 -0400

    Add AprilTag library

--- bottle_recognition/bottle_recognizer.hpp
+++ bottle_recognition/bottle_recognizer.hpp
@@ -22,12 +22,12 @@
 
 #include "opencv2/opencv.hpp"
 
-#include "AprilTags/TagDetector.h"
-#include "AprilTags/Tag16h5.h"
-#include "AprilTags/Tag25h7.h"
-#include "AprilTags/Tag25h9.h"
-#include "AprilTags/Tag36h9.h"
-#include "AprilTags/Tag36h11.h"
+#include "apriltags/TagDetector.h"
+#include "apriltags/Tag16h5.h"
+#include "apriltags/Tag25h7.h"
+#include "apriltags/Tag25h9.h"
+#include "apriltags/Tag36h9.h"
+#include "apriltags/Tag36h11.h"
 
 using namespace std;
 
