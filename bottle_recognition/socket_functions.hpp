602498fd25154d83dd8c2a3c651198b6495b7c82
Steven Ma <7348348564@stevenma.xyz>
2017-03-25 20:31:12 -0400

    Add socket functions declaration

---
+++ bottle_recognition/socket_functions.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <exception>
+#include <stdexcept>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <climits>
+#include <vector>
+#include <cstdint>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+using namespace std;
+
+int setup_socket(int);
+
+int accept_socket(int);
+
+string read_msg(int socket);
+
+void send_msg(int socket, string msg);
