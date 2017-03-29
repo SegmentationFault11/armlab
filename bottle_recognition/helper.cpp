c811de8b7696ecdaea47c99eb99b6ea92951b169
Steven Ma <7348348564@stevenma.xyz>
2017-03-29 12:54:05 -0400

    Actually add helper.hpp and helper.cpp

---
+++ bottle_recognition/helper.cpp
@@ -0,0 +1,40 @@
+#include "helper.hpp"
+
+#ifdef DEBUG
+#define _(x) x
+#else
+#define _(x)
+#endif
+
+const double PI = 3.14159265358979323846;
+const double TWOPI = 2.0*PI;
+
+unsigned get_milli_sec() {
+    return std::chrono::duration_cast<std::chrono::milliseconds>
+        (std::chrono::system_clock::now().time_since_epoch()).count();
+}
+
+double standardRad(double t) {
+    _(cout << "standardRad >> start, time: " << get_milli_sec() << endl;)
+
+    if (t >= 0.) {
+        t = fmod(t+PI, TWOPI) - PI;
+    } else {
+        t = fmod(t-PI, -TWOPI) + PI;
+    }
+
+    _(cout << "standardRad >> end, time: " << get_milli_sec() << endl;)
+    return t;
+}
+
+void wRo_to_euler(const Eigen::Matrix3d& wRo, double& yaw, double& pitch, double& roll) {
+    _(cout << "wRo_to_euler >> start, time: " << get_milli_sec() << endl;)
+
+    yaw = standardRad(atan2(wRo(1,0), wRo(0,0)));
+    double c = cos(yaw);
+    double s = sin(yaw);
+    pitch = standardRad(atan2(-wRo(2,0), wRo(0,0)*c + wRo(1,0)*s));
+    roll  = standardRad(atan2(wRo(0,2)*s - wRo(1,2)*c, -wRo(0,1)*s + wRo(1,1)*c));
+
+    _(cout << "wRo_to_euler >> end, time: " << get_milli_sec() << endl;)
+}
---
+++ bottle_recognition/helper.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cmath>
+#include <chrono>
+
+#include "apriltags/TagDetector.h"
+#include "apriltags/Tag16h5.h"
+
+unsigned get_milli_sec();
+
+double standardRad(double);
+
+void wRo_to_euler(const Eigen::Matrix3d&, double&, double&, double&);
