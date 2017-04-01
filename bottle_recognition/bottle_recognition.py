3c6aa3291ca5b511c2e7bda242037cc8fd77541d
Steven Ma <7348348564@stevenma.xyz>
2017-04-01 17:28:31 -0400

    Add ip argument for python socket

--- bottle_recognition/bottle_recognition.py
+++ bottle_recognition/bottle_recognition.py
@@ -1,12 +1,13 @@
 import socket
+import sys
 
-def get_bottle_locations():
+def get_bottle_locations(ip = 'localhost'):
 	camera_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	camera_socket.connect(('localhost', 12000))
 
 	camera_socket.send("Get Locations" + '\0')
 
-	print camera_socket.recv(512);
+	print camera_socket.recv(512)
 
 def calibrate_slot_locations(calibration_string):
 	camera_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,6 +15,9 @@ def calibrate_slot_locations(calibration_string):
 
 	camera_socket.send("Calibrate Locations " + calibration_string + '\0')
 
-	print camera_socket.recv(512);
+	print camera_socket.recv(512)
 
-get_bottle_locations();
+if len(sys.argv) > 1:
+	get_bottle_locations(sys.argv[1])
+else:
+	get_bottle_locations()
